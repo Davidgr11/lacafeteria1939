9ef73debfe19682151ea0e88f9f6ee69c422d064
davidgr11 <davidygonzalez@hotmail.com>
2025-04-15 16:12:13 -0600

    build: added animations

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const el = entry.target;
+            const animClass = el.dataset.anim;
+            el.classList.remove('animate-on-scroll');
+            el.classList.add('animate__animated', animClass);
+            observer.unobserve(el); // solo una vez
+        }
+    });
+}, {
+    threshold: 0.4
+});
+
+document.querySelectorAll('.animate-on-scroll').forEach(el => {
+    observer.observe(el);
+});
